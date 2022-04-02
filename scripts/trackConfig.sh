e7e51463e9d3c49ccba05f037f11616e922132b8
IPlayZed <pauljouser@protonmail.com>
2022-04-02 13:40:18 +0200

    Renamed Tex folder and added IoT config helper scripts.

---
+++ scripts/trackConfig.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This utility script tracks the IoT configuraton header file.
+# This is useful if one wants to change not the value, but the
+# structure of the configuraton file.
+git update-index --no-assume-unchanged ../project/src/libraries/IoTConfiguration.h
