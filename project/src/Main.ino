a6b5251d37adff9cd443ac741ee0353a29340c9d
IPlayZed <pauljouser@protonmail.com>
2022-04-02 12:54:05 +0200

    Restructued project for successful build via Arduino.

--- project/src/Main.ino
+++ project/src/Main.ino
@@ -1,12 +1,38 @@
-#include "../libs/SerialLogger.h"
-#include "../config/Configuration.h"
+#include <WiFi.h>
+
+#include "libraries/SerialLogger.h"
+#include "libraries/IoTConfiguration.h"
 
 static const BaseType_t PRO_CPU = PRO_CPU_NUM;
 static const BaseType_t APP_CPU = APP_CPU_NUM;
 
-static const char* SSID = WIFI_SSID;
+static const char* SSID = CONFIG_WIFI_SSID;
+static const char* PASSWORD = CONFIG_WIFI_PASSWORD;
 
 static void WiFi_Connect()
 {
     Logger.Info("Trying to connect to " + String(SSID));
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.print(".");
+    }
+    
+    Serial.println();
+    Logger.Info("Connected to " + String(SSID) + " with IP of " + WiFi.localIP().toString());
+
+}
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
 }
---
+++ project/src/libraries/IoTConfiguration.h
@@ -0,0 +1,6 @@
+#define CONFIG_WIFI_SSID "<your_WiFi_SSID_goes_here>"
+#define CONFIG_WIFI_PASSWORD "<your_WiFi_password_goes_here>"
+
+#define CONFIG_AZURE_FQDN "<your_FQDN_goes_here>.azure-devices.net"
+#define CONFIG_AZURE_DEVICE_ID "<your_device_ID_goes_here>"
+#define CONFIG_AZURE_DEVICE_KEY "<your_device_key_goes_here>"
